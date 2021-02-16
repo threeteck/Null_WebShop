f933dd95ebc09833a326314a95866c5d2e244b2f
threeteck <threeteck@users.noreply.github.com>
2021-02-16 18:11:18 +0300

    created asp.net project

---
+++ WebShop_NULL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebShop_NULL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
