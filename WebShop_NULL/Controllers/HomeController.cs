380ac4a2f0648cc16b88057a0f05ec199a3c091c
DanilArkhipov <danilarkhipov00@gmail.com>
2021-03-06 21:21:58 +0300

    Started working on layout and shopping cart

--- WebShop_NULL/Controllers/HomeController.cs
+++ WebShop_NULL/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace WebShop_NULL.Controllers
             return View();
         }
 
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
