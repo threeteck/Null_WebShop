712de55f172449caba745a36c03225c8ecea3daa
DanilArkhipov <danilarkhipov00@gmail.com>
2021-03-08 17:22:13 +0300

    Added personal area button to the header and fixed search dropdown problem

--- WebShop_NULL/wwwroot/js/site.js
+++ WebShop_NULL/wwwroot/js/site.js
@@ -1,4 +1,7 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+function ToggleSearch() {
+    let search = $("#search");
+    search.toggle(200);
+}
 // Write your JavaScript code.
