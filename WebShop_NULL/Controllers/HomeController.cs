d0b284b5e5ef9e61c2567471f5a2e3b2a8db988a
DanilArkhipov <danilarkhipov00@gmail.com>
2021-03-10 21:05:04 +0300

    Added SignIn view and added link to that view in header

--- WebShop_NULL/Controllers/HomeController.cs
+++ WebShop_NULL/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace WebShop_NULL.Controllers
             return View();
         }
 
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
