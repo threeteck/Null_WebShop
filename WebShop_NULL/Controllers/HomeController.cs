02f3af9771604ef0fb9c8f52d63a54a5bbe1b1e5
DanilArkhipov <danilarkhipov00@gmail.com>
2021-03-12 20:23:17 +0300

    Added Orders page

--- WebShop_NULL/Controllers/HomeController.cs
+++ WebShop_NULL/Controllers/HomeController.cs
@@ -44,6 +44,10 @@ namespace WebShop_NULL.Controllers
         {
             return View();
         }
+        public IActionResult Orders()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
