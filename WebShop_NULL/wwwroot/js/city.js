136b007e6bb7cd61dadc822809267f08a7e3ff46
artemgur <artemgur01@gmail.com>
2021-03-16 16:36:36 +0300

    Done manual setting of city

--- WebShop_NULL/wwwroot/js/city.js
+++ WebShop_NULL/wwwroot/js/city.js
@@ -1,11 +1,28 @@
-﻿function getLocation(){
+﻿window.onload = function initCityOnClick() {
+    let cityLi = document.getElementsByClassName('set_city_manual')
+    for (var i = 0; i < cityLi.length; i++){
+        let a = i//just in case, not sure if necessary 
+        let text = cityLi[a].innerHTML
+        cityLi[a].onClick = setCityManualOnClick(text)
+    }
+    if (getCookie('city') === '')
+        setCityAutomatically()
+}
+
+//Gets cookie value by cookie name
+const getCookie = (name) => (
+    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+)
+//Source: https://stackoverflow.com/questions/5639346/what-is-the-shortest-function-for-reading-a-cookie-by-name-in-javascript
+
+function setCityAutomatically(){
     if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(setCityAutomatically)
+        navigator.geolocation.getCurrentPosition(setCityAutomaticallyCallback)
     else
         alert("Geolocation isn't supported")
 }
 
-async function setCityAutomatically(position) {
+async function setCityAutomaticallyCallback(position) {
     let response = await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='
         + position.coords.latitude
         + '&longitude='
@@ -14,7 +31,8 @@ async function setCityAutomatically(position) {
     if (response.ok){
         let json = await response.json()
         let city = json.city
-        setCity(city)
+        //TODO show some popup asking if the city is correct
+        //setCity(city)
         alert(city)//TODO do something useful with the city
     }
     else
@@ -24,4 +42,8 @@ async function setCityAutomatically(position) {
 //Can be used to set city manually
 function setCity(city){
     document.cookie = "city="+city
+}
+
+function setCityManualOnClick(text){
+    setCity(text)
 }
