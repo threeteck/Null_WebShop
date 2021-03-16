d0f5a2297ac73678526b4753e5efcec3344c8260
artemgur <artemgur01@gmail.com>
2021-03-16 15:58:56 +0300

    Added saving city to cookies

--- WebShop_NULL/wwwroot/js/city.js
+++ WebShop_NULL/wwwroot/js/city.js
@@ -1,11 +1,11 @@
 ﻿function getLocation(){
     if (navigator.geolocation)
-        navigator.geolocation.getCurrentPosition(getCity)
+        navigator.geolocation.getCurrentPosition(setCityAutomatically)
     else
         alert("Geolocation isn't supported")
 }
 
-async function getCity(position) {
+async function setCityAutomatically(position) {
     let response = await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='
         + position.coords.latitude
         + '&longitude='
@@ -14,8 +14,14 @@ async function getCity(position) {
     if (response.ok){
         let json = await response.json()
         let city = json.city
+        setCity(city)
         alert(city)//TODO do something useful with the city
     }
     else
         alert("Html error")
+}
+
+//Can be used to set city manually
+function setCity(city){
+    document.cookie = "city="+city
 }
