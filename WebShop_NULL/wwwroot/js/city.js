2f3702c60610dcfe836afb16f35d23668a7afd63
artemgur <artemgur01@gmail.com>
2021-03-16 14:57:20 +0300

    Done js for city reverse geocoding

---
+++ WebShop_NULL/wwwroot/js/city.js
@@ -0,0 +1,21 @@
+﻿function getLocation(){
+    if (navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(getCity)
+    else
+        alert("Geolocation isn't supported")
+}
+
+async function getCity(position) {
+    let response = await fetch('https://api.bigdatacloud.net/data/reverse-geocode-client?latitude='
+        + position.coords.latitude
+        + '&longitude='
+        + position.coords.longitude
+        + '&localityLanguage=ru')
+    if (response.ok){
+        let json = await response.json()
+        let city = json.city
+        alert(city)//TODO do something useful with the city
+    }
+    else
+        alert("Html error")
+}
