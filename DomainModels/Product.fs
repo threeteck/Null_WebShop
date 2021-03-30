1bf7d94abd9a17913375e8f57773671e6a501ff4
artemgur <artemgur01@gmail.com>
2021-03-30 13:22:31 +0300

    Some unfinished progress

---
+++ DomainModels/Product.fs
@@ -0,0 +1,17 @@
+﻿namespace DomainModels
+
+open System.ComponentModel.DataAnnotations;
+//TODO
+[<CLIMutable>]
+type Product =
+    {
+        [<Key>]
+        Id:int
+        Name:string
+        Description:string
+        Price:decimal
+        Rating:decimal
+        
+        CategoryId:int
+        
+    }
