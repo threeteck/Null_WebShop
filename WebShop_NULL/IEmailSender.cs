f0075732a5baa8d90ab9f127b5864a1c2760414e
artemgur <artemgur01@gmail.com>
2021-04-21 12:16:33 +0300

    Resolving merge conflict

---
+++ WebShop_NULL/IEmailSender.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace WebShop_NULL
+{
+	public interface IEmailSender
+	{
+		public Task<bool> SendEmailAsync(string email, string subject, string message);
+	}
+}
