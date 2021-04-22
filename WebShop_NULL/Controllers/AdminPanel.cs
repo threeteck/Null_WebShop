7b835fc3447a3c7ef7e8f51ef10cccc9120a00a3
threeteck <threeteck@users.noreply.github.com>
2021-04-22 13:45:04 +0300

    imporved command service by allowing commands set message and status of execution(success or failure)

--- WebShop_NULL/Controllers/AdminPanel.cs
+++ WebShop_NULL/Controllers/AdminPanel.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebShop_NULL.Models.ViewModels.AdminPanelModels;
 
 namespace WebShop_NULL.Controllers
 {
     public class AdminPanel : Controller
     {
-        private CommandService _commandService;
+        private readonly CommandService _commandService;
 
         public AdminPanel(CommandService commandService)
         {
@@ -15,15 +16,17 @@ namespace WebShop_NULL.Controllers
         [HttpGet("/admin/commandLine")]
         public IActionResult CommandLine()
         {
-            return View(false);
+            return View(CommandLineResponse.Empty());
         }
         
         [HttpPost("/admin/commandLine")]
         public IActionResult CommandLine(string command)
         {
-            if(!_commandService.ExecuteCommand(command))
-                ModelState.AddModelError("", "");
-            return View(true);
+            var response = CommandLineResponse.Empty();
+            if (!_commandService.TryExecuteCommand(command, out var message))
+                response = CommandLineResponse.Failure(message);
+            else response = CommandLineResponse.Success(message);
+            return View(response);
         }
     }
 }
