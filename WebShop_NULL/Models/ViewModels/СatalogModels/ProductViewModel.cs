6d6efe4e1239195283bbd7f35838630de7dabcdd
Timur Abdullin <timur.122@yandex.ru>
2021-04-22 14:47:31 +0300

    Удалил бесполезный select, добавил ProductViewModel, теперь передаю именно его в методе ViewProductPage

---
+++ WebShop_NULL/Models/ViewModels/СatalogModels/ProductViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebShop_NULL.Models.ViewModels.СatalogModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ImageId { get; set; }
+    }
+}
