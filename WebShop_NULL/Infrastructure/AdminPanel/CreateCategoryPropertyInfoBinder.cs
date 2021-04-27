88c88463d072ba41655293d7feef60da2e63f0c8
threeteck <threeteck@users.noreply.github.com>
2021-04-27 15:07:20 +0300

    added category creation feature

---
+++ WebShop_NULL/Infrastructure/AdminPanel/CreateCategoryPropertyInfoBinder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebShop_NULL.Models.ViewModels.AdminPanelModels;
+
+namespace WebShop_NULL.Infrastructure.AdminPanel
+{
+    public class CreateCategoryPropertyInfoBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if(bindingContext == null)
+                throw new ArgumentNullException(nameof(bindingContext));
+
+            var modelName = bindingContext.ModelName;
+            var propertyNameResult = bindingContext.ValueProvider.GetValue($"{modelName}.Name");
+            var propertyTypeResult = bindingContext.ValueProvider.GetValue($"{modelName}.Type");
+            if (propertyNameResult == ValueProviderResult.None ||
+                propertyTypeResult == ValueProviderResult.None ||
+                !int.TryParse(propertyTypeResult.FirstValue, out var propertyType) ||
+                string.IsNullOrWhiteSpace(propertyNameResult.FirstValue) ||
+                propertyType < 0 || propertyType > 2)
+            {
+                ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            if (propertyType == 2)
+            {
+                var propertyOptionsResult = bindingContext.ValueProvider.GetValue($"{modelName}.Options");
+                if (propertyOptionsResult == ValueProviderResult.None)
+                {
+                    ModelBindingResult.Failed();
+                    return Task.CompletedTask;
+                }
+                    
+                var options = propertyOptionsResult.Values.ToList();
+                bindingContext.Result = ModelBindingResult.Success(new CreateCategoryOptionPropertyInfo()
+                {
+                    Name = propertyNameResult.FirstValue,
+                    Type = propertyType,
+                    Options = options
+                });
+            
+                return Task.CompletedTask;
+            }
+            
+            bindingContext.Result = ModelBindingResult.Success(new CreateCategoryPropertyInfo()
+            {
+                Name = propertyNameResult.FirstValue,
+                Type = propertyType
+            });
+            
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebShop_NULL/Models/ViewModels/AdminPanelModels/AdminPanelCreateCategoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using WebShop_NULL.Infrastructure.AdminPanel;
+
+namespace WebShop_NULL.Models.ViewModels.AdminPanelModels
+{
+    [ModelBinder(BinderType = typeof(CreateCategoryPropertyInfoBinder))]
+    public class CreateCategoryPropertyInfo
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+    }
+
+    public class CreateCategoryOptionPropertyInfo : CreateCategoryPropertyInfo
+    {
+        public List<string> Options { get; set; }
+    }
+
+    public class AdminPanelCreateCategoryViewModel
+    {
+        [Required]
+        public string CategoryName { get; set; }
+
+        public List<CreateCategoryPropertyInfo> PropertyInfos { get; set; }
+    }
+}
