45552daf46e8dfc9ebe3d1eb4d1ae369fe001aed
DanilArkhipov <danilarkhipov00@gmail.com>
2021-05-23 19:53:36 +0300

    Implemented page for choosing delivery method

---
+++ WebShop_NULL/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop_FSharp.ViewModels.OrderModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebShop_NULL.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult ChooseDeliveryMethod(OrderSummaryViewModel model)
+        {
+            return View(new OrderSummaryViewModel()
+            {
+                TotalPrice = 77350,
+                TotalCount = 3,
+            });
+        }
+    }
+}
